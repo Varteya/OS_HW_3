c28ff4841b728077d6cfed04a43fad7a9a60cf6e
satamas <se.atamas@gmail.com>
2015-06-13 12:36:45 +0300

    Added wait(NULL);

--- main.c
+++ main.c
@@ -1,29 +1,33 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
+#include <sys/wait.h>
 
 int main()
 {
-    printf("A : %d\n", getpid());
+    printf("A : %d My parent: %d\n", getpid(), getppid());
     int B_fork = fork();
     if (B_fork == 0){
-        printf("B : %d\n", getpid());
+        printf("B : %d My parent: %d\n", getpid(), getppid());
     } else {
+        wait(NULL);
         int C_fork = fork();
         if (C_fork == 0){
-            printf("C : %d\n", getpid());
+            printf("C : %d My parent: %d\n", getpid(), getppid());
             int D_fork = fork();
             if (D_fork == 0){
-                printf("D : %d\n", getpid());
+                printf("D : %d My parent: %d\n", getpid(), getppid());
             } else {
+                wait(NULL);
                 int E_fork = fork();
                 if (E_fork == 0){
-                    printf("E : %d\n", getpid());
+                    printf("E : %d My parent: %d\n", getpid(), getppid());
                 }
             }
+            wait(NULL);
         }
     }
+    wait(NULL); 
     return 0;
 }
 
